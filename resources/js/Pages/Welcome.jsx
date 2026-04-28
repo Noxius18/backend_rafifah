582319f0cef703e35ce0f12f4c214d8c22bd3c05
Noxius18 <mohammad.adamnur@gmail.com>
2026-04-28 09:05:33 +0700

    feat: tambah tailwind

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,17 @@
-export default function Welcome({ nama }) {
+export default function Welcome() {
     return (
-        <h1>Halo {nama} selamat datang di InertiaJS React + Laravel!</h1>
-    )
+        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
+            <div className="p-8 bg-white rounded-xl shadow-2xl">
+                <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-500 mb-4">
+                    Admin Panel TA
+                </h1>
+                <p className="text-slate-500 font-medium text-center">
+                    Tailwind CSS sudah menyala!
+                </p>
+                <button className="mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg transition-colors">
+                    Mulai Jelajahi
+                </button>
+            </div>
+        </div>
+    );
 }
