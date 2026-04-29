0288bfc26548fe6f05605b863587615d7d4b41ca
Noxius18 <mohammad.adamnur@gmail.com>
2026-04-29 17:17:45 +0700

    feat: tambah daisyUI + AlpineJS

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="min-h-screen flex items-center justify-center">
+
+    {{-- DaisyUI component --}}
+    <div class="card w-96 bg-base shadow-xl">
+        <div class="card-body">
+            <h2 class="card-title">Laravel 13 🚀</h2>
+            <p>AlpineJS + DaisyUI + Tailwind 4</p>
+
+            {{-- Alpine JS --}}
+            <div x-data="{ count: 0 }">
+                <p class="text-lg font-bold">Count: <span x-text="count"></span></p>
+                <button class="btn btn-primary" @click="count++">Tambah</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
