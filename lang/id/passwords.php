610edef2fd422b0b29af0c4b3e1d6c53c06a2ae4
Noxius18 <mohammad.adamnur@gmail.com>
2026-04-30 07:20:20 +0700

    feat: tambah bahasa Indonesia locale

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Password kamu sudah direset.',
+    'sent' => 'Kami telah mengirimkan link reset password ke email kamu.',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token reset password tidak valid.',
+    'user' => "Kami tidak dapat menemukan user dengan email tersebut.",
+
+];
